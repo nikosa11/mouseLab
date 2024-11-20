e7227ca14509b7d77664875a89ac57e327d1702e
nlitsakis <nlitsakis@gmail.com>
2024-11-20 17:05:28 +0200

    Added project ID to app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,50 @@
-import { ExpoConfig, ConfigContext } from 'expo/config';
+import { ExpoConfig } from 'expo/config';
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config,
-  name: 'my-first-app',
-  slug: 'my-first-app',
+const config: ExpoConfig = {
+  name: "my-first-app",
+  slug: "my-first-app",
+  scheme: "mouseLab",
+  plugins: [
+    [
+      "expo-notifications",
+      {
+        "icon": "./assets/notification-icon.png",
+        "color": "#ffffff",
+        "sounds": ["./assets/notification-sound.wav"]
+      }
+    ]
+  ],
   extra: {
-    supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+    eas: {
+      projectId: "9b6e78b1-563a-4587-ab8d-b9aadfbf31df"
+    }
   },
-}); 
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  assetBundlePatterns: [
+    "**/*"
+  ],
+  ios: {
+    supportsTablet: true
+  },
+  android: {
+    package: "com.nlitsakis.mouselab",
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    },
+    permissions: ["NOTIFICATIONS"]
+  },
+  web: {
+    favicon: "./assets/icon.png"
+  }
+};
+
+export default config; 
